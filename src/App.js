17c33016c9441c1c41b5735e51367e4fdbabef76
luzhen <luzhenlimengcai@foxmail.com>
2021-05-21 09:35:49 +0800

    提交app修改

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-function App () {
-    return (
-        <div className="App">
-            hellow world
-        </div>
-    )
+import React, { Component } from 'react'
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                hello  world
+            </div>
+        )
+    }
 }
-export default App;
